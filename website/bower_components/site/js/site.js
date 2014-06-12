a76db836f1668ec38efae4e5e714efd46e35681b
Shaun Luttin <shaunluttin@gmail.com>
2014-06-12 13:39:01 -0700

    Start on the main content.

--- website/bower_components/site/js/site.js
+++ website/bower_components/site/js/site.js
@@ -6,8 +6,8 @@
         $("#navigation").append(data);
     });
 
-    $.get("bower_components/site/ajax/main-content.htm", function (data) {
-        $("#main-content").append(data);
+    $.get("bower_components/site/ajax/splash.htm", function (data) {
+        $("#splash").append(data);
     });
 
 });
