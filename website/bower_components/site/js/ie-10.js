5d76f6ddc9034af7e56df61d557556ff0342773d
Shaun Luttin <shaunluttin@gmail.com>
2014-06-12 12:39:19 -0700

    Add navbar and no-limits.png.

---
+++ website/bower_components/site/js/ie-10.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+
+    // Handler for .ready() called.
+
+    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+        var msViewportStyle = document.createElement('style')
+        msViewportStyle.appendChild(
+          document.createTextNode(
+            '@-ms-viewport{width:auto!important}'
+          )
+        )
+        document.querySelector('head').appendChild(msViewportStyle)
+    }
+
+});
+
---
+++ website/bower_components/site/js/site.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+
+    // Handler for .ready() called.
+
+    $.get("bower_components/site/ajax/navbar.htm", function (data) {
+        $("#navigation").append(data);
+    });
+
+    $.get("bower_components/site/ajax/main-content.htm", function (data) {
+        $("#main-content").append(data);
+    });
+
+});
+
