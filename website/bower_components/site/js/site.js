ea0d70593dbfba97aa78ce898eb4d955b4830756
Shaun Luttin <shaunluttin@gmail.com>
2014-06-12 22:35:56 -0700

    Refactor widgets into own files. Refactor css.

--- website/bower_components/site/js/site.js
+++ website/bower_components/site/js/site.js
@@ -1,23 +1,53 @@
-﻿function InjectFakeWidget(widgetName, containerId)
-{
+﻿var SNAKE_CASE_REGEXP = /[A-Z]/g;
+function snake_case(name, separator) {
+    separator = separator || '-';
+    return name.replace(SNAKE_CASE_REGEXP, function (letter, pos) {
+        return (pos ? separator : '') + letter.toLowerCase();
+    });
+}
 
+var SPECIAL_CHARS_REGEXP = /([\:\-\_]+(.))/g;
+var MOZ_HACK_REGEXP = /^moz([A-Z])/;
+function camelCase(name) {
+    return name.
+      replace(SPECIAL_CHARS_REGEXP, function (_, separator, letter, offset) {
+          return offset ? letter.toUpperCase() : letter;
+      }).
+      replace(MOZ_HACK_REGEXP, 'Moz$1');
+}
+
+function InjectFakeWidget(widgetContainer) {
+
+    var widgetContainer = $(widgetContainer);
+    var classes = widgetContainer.attr('class').split(/\s+/);;
+    var widgetClass = classes[1]; // todo Do this more accurately
+    var widgetName = camelCase(widgetClass.replace('widget-', ''));
+
+    $.get('bower_components/site/ajax/widgets/' + widgetName + '.html?foo=bar', function (data) {
+        if (data.length > 0) {
+            widgetContainer.append(data);
+        }
+    });
 }
 
 function InjectFakeZone(zoneName, containerId) {
 
     var container = $('#' + containerId);
-    var zone = $('<div>', { 'class': 'zone' });    
-    
+    var zone = $('<div>', { 'class': 'zone zone-' + snake_case(zoneName) });
+
     container.empty();
 
     $.get('bower_components/site/ajax/' + zoneName + '.html', function (data) {
-        
-        if (data.length > 0)
-        {
+
+        if (data.length > 0) {
+
             container.append(zone);
             zone.append(data);
-            console.debug('appending ' + zoneName);
-        }        
+
+            zone.find('.widget').each(function () {
+                InjectFakeWidget(this);
+            });
+        }
     });
 }
 
