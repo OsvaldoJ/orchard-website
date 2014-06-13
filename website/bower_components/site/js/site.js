b494a11f5331f9d0767c03004b7ef694cc2e1903
Shaun Luttin <shaunluttin@gmail.com>
2014-06-12 20:25:44 -0700

    Use html not htm.

--- website/bower_components/site/js/site.js
+++ website/bower_components/site/js/site.js
@@ -1,15 +1,23 @@
-﻿function InjectPsuedoZone(zoneName, containerId) {
+﻿function InjectFakeWidget(widgetName, containerId)
+{
+
+}
+
+function InjectFakeZone(zoneName, containerId) {
 
     var container = $('#' + containerId);
     var zone = $('<div>', { 'class': 'zone' });    
     
     container.empty();
 
-    $.get('bower_components/site/ajax/' + zoneName + '.htm', function (data) {
+    $.get('bower_components/site/ajax/' + zoneName + '.html', function (data) {
         
-        container.append(zone);
-        zone.append(data);
-
+        if (data.length > 0)
+        {
+            container.append(zone);
+            zone.append(data);
+            console.debug('appending ' + zoneName);
+        }        
     });
 }
 
@@ -17,26 +25,25 @@ $(function () {
 
     // Handler for .ready() called.
 
-    InjectPsuedoZone('Header', 'header');
-    InjectPsuedoZone('Navigation', 'layout-navigation');
-    InjectPsuedoZone('Featured', 'layout-featured');
-    InjectPsuedoZone('BeforeMain', 'layout-before-main');
-    InjectPsuedoZone('AsideFirst', 'aside-first');
-    InjectPsuedoZone('Messages', 'messages');
-    InjectPsuedoZone('BeforeContent', 'before-content');
-    InjectPsuedoZone('Content', 'content');
-    InjectPsuedoZone('AfterContent', 'after-content');
-    InjectPsuedoZone('AsideSecond', 'aside-second');
-    InjectPsuedoZone('AfterMain', 'layout-after-main');
-    InjectPsuedoZone('TripelFirst', 'tripel-first');
-    InjectPsuedoZone('TripelSecond', 'tripel-second');
-    InjectPsuedoZone('TripelThird', 'tripel-third');
-    InjectPsuedoZone('FooterQuadFirst', 'footer-quad-first');
-    InjectPsuedoZone('FooterQuadSecond', 'footer-quad-second');
-    InjectPsuedoZone('FooterQuadThird', 'footer-quad-third');
-    InjectPsuedoZone('FooterQuadFourth', 'footer-quad-fourth');
-    InjectPsuedoZone('Footer', 'footer-sig');
-
+    InjectFakeZone('Header', 'header');
+    InjectFakeZone('Navigation', 'layout-navigation');
+    InjectFakeZone('Featured', 'layout-featured');
+    InjectFakeZone('BeforeMain', 'layout-before-main');
+    InjectFakeZone('AsideFirst', 'aside-first');
+    InjectFakeZone('Messages', 'messages');
+    InjectFakeZone('BeforeContent', 'before-content');
+    InjectFakeZone('Content', 'content');
+    InjectFakeZone('AfterContent', 'after-content');
+    InjectFakeZone('AsideSecond', 'aside-second');
+    InjectFakeZone('AfterMain', 'layout-after-main');
+    InjectFakeZone('TripelFirst', 'tripel-first');
+    InjectFakeZone('TripelSecond', 'tripel-second');
+    InjectFakeZone('TripelThird', 'tripel-third');
+    InjectFakeZone('FooterQuadFirst', 'footer-quad-first');
+    InjectFakeZone('FooterQuadSecond', 'footer-quad-second');
+    InjectFakeZone('FooterQuadThird', 'footer-quad-third');
+    InjectFakeZone('FooterQuadFourth', 'footer-quad-fourth');
+    InjectFakeZone('Footer', 'footer-sig');
 
 });
 
