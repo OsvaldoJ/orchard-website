d2fa167620bbf1f05537ed64498969e4ac8e8009
Shaun Luttin <shaunluttin@gmail.com>
2014-06-12 17:56:33 -0700

    Add up to features in alpha.

--- website/bower_components/site/js/site.js
+++ website/bower_components/site/js/site.js
@@ -1,14 +1,42 @@
-﻿$(function () {
+﻿function InjectPsuedoZone(zoneName, containerId) {
+
+    var container = $('#' + containerId);
+    var zone = $('<div>', { 'class': 'zone' });    
+    
+    container.empty();
+
+    $.get('bower_components/site/ajax/' + zoneName + '.htm', function (data) {
+        
+        container.append(zone);
+        zone.append(data);
+
+    });
+}
+
+$(function () {
 
     // Handler for .ready() called.
 
-    $.get("bower_components/site/ajax/navbar.htm", function (data) {
-        $("#navigation").append(data);
-    });
+    InjectPsuedoZone('Header', 'header');
+    InjectPsuedoZone('Navigation', 'layout-navigation');
+    InjectPsuedoZone('Featured', 'layout-featured');
+    InjectPsuedoZone('BeforeMain', 'layout-before-main');
+    InjectPsuedoZone('AsideFirst', 'aside-first');
+    InjectPsuedoZone('Messages', 'messages');
+    InjectPsuedoZone('BeforeContent', 'before-content');
+    InjectPsuedoZone('Content', 'content');
+    InjectPsuedoZone('AfterContent', 'after-content');
+    InjectPsuedoZone('AsideSecond', 'aside-second');
+    InjectPsuedoZone('AfterMain', 'layout-after-main');
+    InjectPsuedoZone('TripelFirst', 'tripel-first');
+    InjectPsuedoZone('TripelSecond', 'tripel-second');
+    InjectPsuedoZone('TripelThird', 'tripel-third');
+    InjectPsuedoZone('FooterQuadFirst', 'footer-quad-first');
+    InjectPsuedoZone('FooterQuadSecond', 'footer-quad-second');
+    InjectPsuedoZone('FooterQuadThird', 'footer-quad-third');
+    InjectPsuedoZone('FooterQuadFourth', 'footer-quad-fourth');
+    InjectPsuedoZone('Footer', 'footer-sig');
 
-    $.get("bower_components/site/ajax/splash.htm", function (data) {
-        $("#splash").append(data);
-    });
 
 });
 
