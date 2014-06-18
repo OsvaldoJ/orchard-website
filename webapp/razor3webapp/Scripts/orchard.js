c7c2174cebe51450fddeed0eac3f7afd4d5bf076
Shaun Luttin <shaun@bigfont.ca>
2014-06-18 19:00:48 -0400

    Make the Why Orchard carousel indicators follow the active item.

---
+++ webapp/razor3webapp/Scripts/orchard.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    // Handler for .ready() called.
+
+    var indicators = $('#why-orchard').find("ol li");
+
+    $('#carousel-why-orchard')
+        .on('slide.bs.carousel', function () {
+            indicators.removeClass("active");
+        }).on('slid.bs.carousel', function () {
+            var activeItem,
+                activeIndex,
+                items;
+            items = $(this).find('.item');
+            activeItem = $(this).find('.item.active');
+            activeIndex = items.index(activeItem);
+            indicators.eq(activeIndex).addClass("active");
+        });
+});
