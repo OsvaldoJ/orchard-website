083e0d0a45fa0e22e5fd6bf0c4e8c3c38d0711ca
Shaun Luttin <shaun@bigfont.ca>
2014-09-23 17:01:03 -0700

    Let end users toggle the base gray colour.

--- webapp/razor3webapp/Scripts/orchard.js
+++ webapp/razor3webapp/Scripts/orchard.js
@@ -1,18 +1,50 @@
-﻿$(function () {
-    // Handler for .ready() called.
+﻿(function (window) {
 
-    var indicators = $('#why-orchard').find("ol li");
+    function setupWhyOrchardCarousel() {
+        var indicators = $('#why-orchard').find("ol li");
+        $('#carousel-why-orchard')
+            .on('slide.bs.carousel', function () {
+                indicators.removeClass("active");
+            }).on('slid.bs.carousel', function () {
+                var activeItem,
+                    activeIndex,
+                    items;
+                items = $(this).find('.item');
+                activeItem = $(this).find('.item.active');
+                activeIndex = items.index(activeItem);
+                indicators.eq(activeIndex).addClass("active");
+            });
+    }
 
-    $('#carousel-why-orchard')
-        .on('slide.bs.carousel', function () {
-            indicators.removeClass("active");
-        }).on('slid.bs.carousel', function () {
-            var activeItem,
-                activeIndex,
-                items;
-            items = $(this).find('.item');
-            activeItem = $(this).find('.item.active');
-            activeIndex = items.index(activeItem);
-            indicators.eq(activeIndex).addClass("active");
+    // see http://lesscss.org/usage/#using-less-in-the-browser
+    function setupLessColourPicker() {
+
+        var key, value, obj, msg, input;
+
+        msg = $('#less-colour-picker #msg');
+        input = $('#less-colour-picker input');
+
+        input.change(function () {
+
+            msg.show();
+
+            key = $(this).attr('name');
+            value = $(this).val();
+            obj = {};
+            obj[key] = value;
+
+            window.setTimeout(function () {
+                less.modifyVars(obj);
+                msg.fadeOut('slow');
+            }, 500);
+
         });
-});
+
+        input.change();
+    }
+
+    setupWhyOrchardCarousel();
+    setupLessColourPicker();
+
+}(window));
+
